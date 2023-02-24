cdc313753bf7a8928f9acde2b3cfbfb9dea8b0fe
ELFoff10 <andrei.kaliadzin@gmail.com>
2023-02-24 12:00:22 +0300

    24.02.2023 12-00

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,18 +5,23 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float m_Speed = 3f;
     [SerializeField] private float m_BonusRunSpeed = 3f;
-    //[SerializeField] private float m_JumpHeight = 6.5f;
+    [SerializeField] private float m_JumpHeight = 6.5f;
+    [SerializeField] private GameObject m_JumpToPointLeft;
+    [SerializeField] private GameObject m_JumpToPointRight;
 
     private Rigidbody2D m_Rigidbody2D;
     private Vector2 m_MoveVelocity;
     private SpriteRenderer m_SpriteRenderer;
     private Animator m_Animator;
+    private bool m_FacingRight = true;
+    private float m_DefoultSpeed;
 
     private void Start()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
         m_SpriteRenderer = GetComponentInChildren<SpriteRenderer>();
         m_Animator = GetComponent<Animator>();
+        m_DefoultSpeed = m_Speed;
     }
 
     private void Update()
@@ -27,36 +32,63 @@ public class Player : MonoBehaviour
 
         m_Rigidbody2D.MovePosition(m_Rigidbody2D.position + m_MoveVelocity * Time.deltaTime);
 
-        var xMotion = m_Rigidbody2D.velocity.x;
+        //var xMotion = m_Rigidbody2D.velocity.x;
 
-        if (xMotion > 0.01f)
-        {
-            m_SpriteRenderer.flipX = false;
-        }
-        else if (xMotion < 0.01f)
-        {
-            m_SpriteRenderer.flipX = true;
-        }
+        //if (xMotion > 0.01f)
+        //{
+        //    m_SpriteRenderer.flipX = false;
+        //}
+        //else if (xMotion < 0.01f)
+        //{
+        //    m_SpriteRenderer.flipX = true;
+        //}
 
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))/*Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0*/
         {
-            m_Animator.SetBool("isRunning", true);
+            m_Animator.SetBool("Run", true);
         }
         else
         {
-            m_Animator.SetBool("isRunning", false);
+            m_Animator.SetBool("Run", false);
         }
 
-        if (Input.GetKey(KeyCode.LeftShift) == true)
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_Animator.SetTrigger("Jump");
+
+            //if (true)
+            //{
+
+            //}
+            //transform.position = Vector3.Slerp(transform.position, )
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) == true)
         {
             m_Speed += m_BonusRunSpeed;
             // Тут можно включить анимацию бега
+
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift) == true)
+        {
+            m_Speed = m_DefoultSpeed;
         }
 
-        //    // Jumping
-        //    if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        //    {
-        //        r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
-        //    }       
+        if (m_FacingRight == false && Input.GetAxisRaw("Horizontal") > 0)
+        {
+            Flip();
+        }
+        if (m_FacingRight == true && Input.GetAxisRaw("Horizontal") < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
     }
 }
